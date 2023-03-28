7dbb76c61d2fece813d868412a989151f8ff6e7e
unknown <you@example.c>
2023-03-28 13:53:13 +0600

    ver

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, 'build/index.html')],
 
         'APP_DIRS': True,
         'OPTIONS': {
